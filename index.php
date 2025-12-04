117be31e0f026d408e0a2ec30a7df2d752c68942
MohamedRahoumi <mohamedrahoumi4@gmail.com>
2025-12-04 21:57:33 +0100

    ajouter le array pour les titres

--- index.php
+++ index.php
@@ -8,6 +8,15 @@ $routes = [
 
 $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 
+$page = $routes[$uri] ?? '404';
+
+$titles = [
+    'home'     => 'Accueil - DigitalWave',
+    'services' => 'Nos Services - DigitalWave',
+    'about'    => 'A propos - DigitalWave',
+    'contact'  => 'Contact - DigitalWave',
+    '404'      => 'Page introuvable - DigitalWave'
+];
 
 
 
