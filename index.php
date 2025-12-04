c7ad2078b5a378b2bbfd4e87b6368894db1b529f
MohamedRahoumi <mohamedrahoumi4@gmail.com>
2025-12-04 21:56:45 +0100

    impoter le url est stocke dans

--- index.php
+++ index.php
@@ -1,11 +1,15 @@
 <?php 
-
 $routes = [
     '/'         => 'home',
     '/services' => 'services',
     '/about'    => 'about',
     '/contact'  => 'contact',
 ];
+
+$uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+
+
+
 
 ?>
 
