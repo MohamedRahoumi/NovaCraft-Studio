eeaeee574ea2386a4b56d4d370b8f89bf502be21
MohamedRahoumi <mohamedrahoumi4@gmail.com>
2025-12-05 18:51:59 +0100

    ajout le lien pour retour a page acceuil

--- views/404.view.php
+++ views/404.view.php
@@ -2,5 +2,8 @@
     <h1 class="text-5xl font-bold text-red-600 mb-4">404</h1>
     <p class="text-gray-700 text-lg mb-6">La page que vous recherchez n'existe pas.</p>
     
-    
+    <a href="/?page=home" 
+       class="px-6 py-3 bg-blue-600 text-white rounded-lg hover:bg-blue-700">
+        Retour à l'accueil
+    </a>
 </section>
