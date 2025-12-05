d0eef97a53fe7bd35bd458facd9622f738e000de
MohamedRahoumi <mohamedrahoumi4@gmail.com>
2025-12-05 18:51:17 +0100

    ajout le section de page

---
+++ views/404.view.php
@@ -0,0 +1,6 @@
+<section class="container mx-auto py-20 text-center">
+    <h1 class="text-5xl font-bold text-red-600 mb-4">404</h1>
+    <p class="text-gray-700 text-lg mb-6">La page que vous recherchez n'existe pas.</p>
+    
+    
+</section>
