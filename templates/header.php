37bfbd940134a33d344df132e4f7a3f49fe066c2
MohamedRahoumi <mohamedrahoumi4@gmail.com>
2025-12-05 18:39:57 +0100

    ajouter la structure html

--- templates/header.php
+++ templates/header.php
@@ -2,3 +2,15 @@
 $lienClick = $page;
 ?>
 
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?= $pageTitle ?></title>
+    <script src="https://cdn.tailwindcss.com"></script>
+</head>
+
+<body class="bg-gray-100">
+  
+
