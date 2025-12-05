bf072c0baaf92b308a160e01aa98b964cc3ffd88
MohamedRahoumi <mohamedrahoumi4@gmail.com>
2025-12-05 19:00:16 +0100

    section nos services

---
+++ views/services.view.php
@@ -0,0 +1,8 @@
+<section class="container mx-auto py-16">
+  <h2 class="text-3xl font-bold mb-8 text-center">Nos Services</h2>
+  <div class="grid md:grid-cols-3 gap-8">
+  
+
+  </div>
+</section>
+
